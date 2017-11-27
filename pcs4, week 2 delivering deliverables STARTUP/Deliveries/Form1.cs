9f03e947972b07904e1f061e3e373e7987c6c5c5
LAPTOP-GP5DDMO7\kazik <kazikwaraksa@hotmail.com>
2017-11-27 22:19:57 +0100

    Newst sync

---
+++ pcs4, week 2 delivering deliverables STARTUP/Deliveries/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Deliveries
+{
+    public partial class Form1 : Form
+    {
+
+        string dataFilePersonsLocation = @"C:\Users\kazik\OneDrive\Documenten\Visual Studio 2015\Projects\pcs4, week 2 delivering deliverables STARTUP\Deliveries\data\persons.txt";
+        string dataFileDeliverablesLocation = @"C:\Users\kazik\OneDrive\Documenten\Visual Studio 2015\Projects\pcs4, week 2 delivering deliverables STARTUP\Deliveries\data\deliverables.txt";
+        TransportCompany TC;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btLoad_Click(object sender, EventArgs e)
+        {
+            TC = new TransportCompany();
+
+            try
+            {
+                TC.LoadPersonsFromFile(dataFilePersonsLocation);
+                TC.LoadDeliverablesFromFile(dataFileDeliverablesLocation);
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+
+            listBox1. = TC.Deliverables;
+        }
+
+        private void btSortByWeight_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.DataSource = TC.SortByWeight();
+        }
+
+        private void btSortByName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSortById_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSortByAddress_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSortForPostman_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ pcs4, week 2 delivering deliverables STARTUP/Deliveries/TransportCompany.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Deliveries
+{
+    class TransportCompany
+    {
+        private List<Person> myPersons;
+        private List<Deliverable> myDeliverables;
+
+        public TransportCompany()
+        {
+            this.myPersons = new List<Person>();
+            this.myDeliverables = new List<Deliverable>();
+        }
+
+        public List<Person> Persons { get { return this.myPersons; } }
+        public List<Deliverable> Deliverables { get { return this.myDeliverables; } }
+        
+        public Person FindPerson(int id)
+        {
+            foreach (Person p in this.myPersons)
+            {
+                if (p.ID == id)
+                {
+                    return p;
+                }
+            }
+            return null;
+        }
+
+        public void AddPerson(Person p)
+        {
+            if (this.FindPerson(p.ID) == null)
+            {
+                this.myPersons.Add(p);
+            }
+            else
+            {
+                throw new Exception("Be aware: nothing is added!!!");
+            }
+        }
+
+        public Deliverable FindDeliverable(int id)
+        {
+            foreach (Deliverable d in this.myDeliverables)
+            {
+                if (d.ID == id)
+                {
+                    return d;
+                }
+            }
+            return null;
+        }
+
+        public void AddDeliverable(Deliverable d)
+        {
+            if (FindDeliverable(d.ID) == null)
+            {
+                myDeliverables.Add(d);
+            }
+            else
+            {
+                throw new Exception("Be aware: nothing is added!!!");
+            }
+        }
+
+        public void LoadPersonsFromFile(string filename)
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader(new FileStream(filename, FileMode.Open, FileAccess.Read));
+                this.myPersons.Clear();
+                String name, street, postalcode, city;
+                int id, housenr;
+                name = sr.ReadLine();
+                while (name != null)
+                {
+                    street = sr.ReadLine();
+                    housenr = Convert.ToInt32(sr.ReadLine());
+                    postalcode = sr.ReadLine();
+                    city = sr.ReadLine();
+                    id = Convert.ToInt32(sr.ReadLine());
+                    this.myPersons.Add(new Person(id, name, street, housenr, postalcode, city));
+                    name = sr.ReadLine();
+                    name = sr.ReadLine(); //and again read a line, because of the delimiter between persons(line with the stars)
+                }
+            }
+            catch (IOException) { }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+
+        public void LoadDeliverablesFromFile(string filename)
+        {
+            List<string> Lines = new List<string>();
+
+            using (var stream = new FileStream(filename, FileMode.Open))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        int id, weight, buyerID;
+                        var line = reader.ReadLine();
+                        Lines.Add(line);
+
+                        foreach (string dataLine in Lines)
+                        {
+                            string[] splitLines = dataLine.Split(' ');
+                            
+                            id = Convert.ToInt32(splitLines[0]);
+                            weight = Convert.ToInt32(splitLines[1]);
+                            buyerID = Convert.ToInt32(splitLines[2]);
+                            Person selectedPerson = myPersons.FirstOrDefault(x => x.ID == buyerID);
+
+                            myDeliverables.Add(new Deliverable(id, weight, selectedPerson));   
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// SortByWeight sorts the elements of the deliverables-list from a low weight to a high weight.
+        /// </summary>
+        public void SortByWeight()
+        {
+        }
+
+        /// <summary>
+        /// SortByName sorts the elements of the deliverables-list alfabetically by buyer's name.
+        /// </summary>
+        public void SortByName()
+        {
+            //todo
+
+        }
+
+        /// <summary>
+        /// SortById sorts the elements of the deliverables-list from a low id to a high id.
+        /// </summary>
+        public void SortById()
+        {
+            //todo
+
+        }
+
+        /// <summary>
+        /// SortByAddress sorts the elements of the deliverables-list "alfabetically by street; then by increasing house-number".
+        /// So elements with a different street are sorted alfabetically by street.
+        /// Elements with the same street are sorted by increasing house-number. 
+        /// </summary>
+        public void SortByAddress()
+        {
+            //todo
+
+        }
+
+        /// <summary>
+        /// SortForPostman sorts the elements of the deliverables-list alfabetically by street,
+        /// then by postman-ordered house-numbers.
+        /// The postman walks along the street, first the side of the street with the odd numbers (in an increasing order),
+        /// then back on the other side of the street for the even house-numbers (in a decreasing order). 
+        /// </summary>
+        public void SortForPostman()
+        {
+            //todo
+
+        }
+
+        
+    }
+}
